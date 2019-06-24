4c3a7ca1a648808d653067ef6cb5279e48c077a9
Jian Raymond Li <rayli@fb.com>
2019-06-23 22:04:15 -0700

    create subdirectory for the MixingMilk problem

--- Bronze/MixingMilk/mixmilk.py
+++ Bronze/MixingMilk/mixmilk.py
@@ -3,17 +3,17 @@
 with open("mixmilk.in", "r") as f:
     input_content = f.read()
     input_lines = input_content.split('\n')
-    cows = []
-    buckets = []
+    cap_ar = []
+    milk_ar = []
     for i in range(3):
 	line_items = input_lines[i].split(' ')
-        cow = line_items[0]
-	bucket = line_items[1]
-	cows.append(cow)
-  	buckets.append(bucket)
+        cap = int(line_items[0])
+	milk = int(line_items[1])
+	cap_ar.append(cap)
+  	milk_ar.append(milk)
 
 
 for i in range(3):
-    print(cows[i], buckets[i])
+    print(cap_ar[i], milk_ar[i])
 
 
