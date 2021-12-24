11ef6d01dcd4e0606d687a1c50ba66c1696ad035
Ray Li <jrayli@amazon.com>
2021-12-23 18:51:09 -0800

    Adding code for handling IO for USACO problems

---
+++ C++Drills/basic-io/BasicIo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+int N, K;
+ifstream fin("input.txt");
+
+int main()
+{
+  fin >> N >> K;
+  cout << "N:" << N << endl;
+  cout << "K:" << K << endl;
+
+
+}
+
+
