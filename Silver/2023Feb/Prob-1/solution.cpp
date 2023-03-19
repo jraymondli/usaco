8aea9720143157c4f6d84352462a9e0fc6552762
Ray Li <jrayli@amazon.com>
2023-03-18 17:30:56 -0700

    Committed a fix to GregSolution.cpp in solution.cpp

--- Silver/2023Feb/Prob-1/solution.cpp
+++ Silver/2023Feb/Prob-1/solution.cpp
@@ -12,33 +12,26 @@ using namespace std;
 
 int T;
 int N, X, Y;
-long aa[100], ba[100];
-long long ca[100];
+long long friends[100][3];
 
 bool check(int w){
     //1 <= x <= X
     //1 <= y <= Y
     //x + y = X + Y - w
     //x = X + Y - y - w
-    long lx = max(1, X - w), hx = min(X + Y - w - 1, X);
+    long long lx = max(1, X - w), hx = min(X + Y - w - 1, X);
     for (int i = 0; i < N; i++){
-        long a = aa[i]; long b = ba[i]; long long c = ca[i];
-        long d = X + Y - w;
+        long long a = friends[i][0], b = friends[i][1], c = friends[i][2];
+        long long d = X + Y - w;
         //a * x + b * y <= c
         //x + y = d = (X + Y - w)
         //-b * x - b * y <= -b * d
         //(a-b) * x <= c - b * d
-        if (b - a > 0) {
-	    long tmp = (-c + b * d + (b - a - 1)) / (b - a);
-            lx = max(lx, tmp);
-        } else if (a - b > 0) {
-	    long tmp = (c - b * d) / (a - b);
-            hx = min(hx, tmp);
-        } else {
-	    if (a * d > c) {
-		return false;
-	    }
-	}
+        if (b - a > 0)
+            lx = max(lx, (-c + b * d + (b - a - 1)) / (b - a));
+        else if (a - b > 0)
+            hx = min(hx, (c - b * d) / (a - b));
+        else if (a * d > c) return false;
     }
     return (lx <= hx);
     
@@ -51,11 +44,11 @@ int main() {
     for (int i = 0; i < T; i++){
         cin >> N >> X >> Y;
         for (int j = 0; j < N; j++){
-            cin >>  aa[j] >> ba[j] >> ca[j]; 
+            cin >> friends[j][0] >> friends[j][1] >> friends[j][2];
         }
-        int lo = 0;
-        int hi = X + Y - 2;
-        int mid; 
+        long long lo = 0;
+        long long hi = X + Y - 2;
+        long long mid;
         while(hi > lo){
             mid = (lo + hi) / 2;
             if (check(mid)) hi = mid;
