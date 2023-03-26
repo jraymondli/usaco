0120ab561d34612f16dc8707351e8f24a01158bc
Ray Li <jrayli@amazon.com>
2023-03-25 19:19:39 -0700

    Adding the drill problem for implementing bisect_right in C++

---
+++ C++Drills/bisect/test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+#include <tuple>
+#include <list>
+#include <iterator>
+
+using namespace std;
+
+int bisect_right(int * ar, int n)
+{
+    return 0;
+}
+
+int main()
+{
+   int ar[100];
+   for (int i = 0; i < 100; i++) {
+       ar [i] = i * 2;
+   }
+   cout << bisect_right(ar, 37) << endl;
+
+}
+
