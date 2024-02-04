12ca842ebcbf587ee359734a4adfebe2657400fa
Ray Li <jrayli@amazon.com>
2024-02-04 15:28:55 -0800

    updating a c++ drill

--- C++Drills/bisect/test.cpp
+++ C++Drills/bisect/test.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
 #include <vector>
 #include <tuple>
 #include <list>
 #include <iterator>
+#include <type_traits>
 
 using namespace std;
 
-int bisect_right(int * ar, int n)
+int bisect_right(int ar[], int n, int s)
 {
-    return 0;
+    int l = 0, r = s, m;
+    while (r>l){
+        m = (l+r)/2;
+        if (ar[m]<=n) {
+            l=m+1;
+        } else if (ar[m]>n) {
+            r=m;
+        }
+    }
+    return m+1;
 }
 
 int main()
@@ -18,7 +28,7 @@ int main()
    for (int i = 0; i < 100; i++) {
        ar [i] = i * 2;
    }
-   cout << bisect_right(ar, 37) << endl;
+   cout << bisect_right(ar, -1, sizeof(ar)/sizeof(ar[0])) << endl;
 
 }
 
